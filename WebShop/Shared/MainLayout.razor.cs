74a595c2fe82fc3c1365bd840554c6682a2a12a3
Réka <Rka@DESKTOP-GK7K1C8>
2022-04-15 19:27:43 +0200

    Code separated from UI

---
+++ WebShop/Shared/MainLayout.razor.cs
@@ -0,0 +1,28 @@
+﻿using MatBlazor;
+
+namespace WebShop.Shared
+{
+    public partial class MainLayout
+    {
+    string myStringValue;
+
+    MatTheme theme1 = new MatTheme()
+    {
+        Primary = "#bcaaa4",
+        Secondary = "#bdbdbd"
+    };
+
+        void HomeOnClick()
+        {
+            NavManager.NavigateTo("");
+        }
+
+        bool loggedIn = false;
+
+        void profileOnClick()
+        {
+            string? page = loggedIn ? "/profile" : "/login";
+            NavManager.NavigateTo(page);
+        }
+    }
+}
